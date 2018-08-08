d0958f7e985ef0cb54b1ba1fd733e3ccc5aea7c7
Zilong Huang <843133702@qq.com>
2018-08-08 15:25:19 +0800

    Update roi_align_kernel.cu

--- encoding/lib/gpu/roi_align_kernel.cu
+++ encoding/lib/gpu/roi_align_kernel.cu
@@ -2,6 +2,7 @@
 
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include "ATen/cuda/CUDAContext.h"
 
 namespace {
 
@@ -375,7 +376,7 @@ at::Tensor ROIAlignForwardCUDA(
       <<<ROI_GET_BLOCKS(count),
          ROI_CUDA_NUM_THREADS,
          0,
-         at::globalContext().getCurrentCUDAStream()>>>(
+         at::cuda::getCurrentCUDAStream()>>>(
           count,
           input.data<scalar_t>(),
           static_cast<scalar_t>(spatial_scale),
@@ -422,7 +423,7 @@ at::Tensor ROIAlignBackwardCUDA(
       <<<ROI_GET_BLOCKS(count),
          ROI_CUDA_NUM_THREADS,
          0,
-         at::globalContext().getCurrentCUDAStream()>>>(
+         at::cuda::getCurrentCUDAStream()>>>(
           count,
           grad_output.data<scalar_t>(),
           num_rois,
